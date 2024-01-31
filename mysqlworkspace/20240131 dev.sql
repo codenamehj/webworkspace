69e38ac45871556682ca7cf576851b116340211d
admin <admin@DESKTOP-9K6N0AO>
2024-01-31 17:45:06 +0900

    SQL node.js 복습 과제

--- mysqlworkspace/20240131 dev.sql
+++ mysqlworkspace/20240131 dev.sql
@@ -14,3 +14,14 @@ FROM customers;
 2. MySQL 실행요청 -> js
 3. app.js -> js
 */
+
+CREATE TABLE `t_users` (
+    `user_no` INT AUTO_INCREMENT,
+    `user_id` VARCHAR(100) NOT NULL,
+    `user_pwd` VARCHAR(100) NOT NULL,
+    `user_name` VARCHAR(100) NOT NULL,
+    `user_gender` CHAR(1) CHECK (user_gender IN ('M' , 'F')),
+    `user_age` INT,
+    `join_date` DATE,
+    PRIMARY KEY (`user_no`)
+);
